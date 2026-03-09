fb776a33b79f15ce88a2ab9bb1e8115ad005c14b
antoniosgk <ant.gk2@gmail.com>
2026-03-09 10:58:10 +0000

    9/3

---
+++ plot_csv.py
@@ -0,0 +1,28 @@
+#%%
+import matplotlib.pyplot as plt
+import pandas as pd
+#%%
+#PUT THE INFO YOU WANT
+species="O3"
+name_station="1006A"
+units='ppb'
+#%%
+df=pd.read_csv(f"/home/agkiokas/CAMS/plots/{name_station}_{species}_A_30min.csv")
+print(df)
+print(df.describe())
+print(df.columns)
+#%%
+df1=df.copy()
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+df1 = df.groupby('timestamp').mean('center_ppb')
+print(df1)
+# %%
+plt.figure(figsize=(10, 5))
+plt.plot(df1.index, df1['center_ppb'], label='Mean center_ppb')
+plt.title(f'Timeseries of {species} for the cell where {name_station} station falls into')
+plt.xlabel('Time')
+plt.ylabel(f'{species} {units}')
+plt.xticks(rotation=45)
+plt.legend()
+plt.show()
+# %%
